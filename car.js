0f83565d72d0a498575e69068502800877ce5fe6
Mitchell Foote <mcfoote321@gmail.com>
2022-09-29 22:25:28 -0400

    flesh out code

--- car.js
+++ car.js
@@ -4,6 +4,26 @@ class Car {
         this.y = y;
         this.width = width;
         this.height = height;
+
+        this.speed = 0;
+        this.acceleration = 0.2;
+
+        this.controls = new Controls();
+    }
+
+    update() {
+        if(this.controls.forward) {
+            this.y -= 2;
+        }
+        if(this.controls.reverse) {
+            this.y += 2;
+        }
+        if(this.controls.left) {
+            this.x -= 2;
+        }
+        if(this.controls.right) {
+            this.x += 2;
+        }
     }
 
     draw(ctx) {
