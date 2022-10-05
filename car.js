17da8b1920e4f5c302ab4abe31938d2a1636bdfb
Mitchell Foote <mcfoote321@gmail.com>
2022-10-04 22:07:19 -0400

    add car logic

--- car.js
+++ car.js
@@ -1,5 +1,6 @@
 class Car {
     constructor(x, y, width, height) {
+
         this.x = x;
         this.y = y;
         this.width = width;
@@ -7,23 +8,30 @@ class Car {
 
         this.speed = 0;
         this.acceleration = 0.2;
+        this.maxSpeed = 3;
+        this.friction = 0.05;
 
         this.controls = new Controls();
+
     }
 
     update() {
+
         if(this.controls.forward) {
-            this.y -= 2;
+            this.speed += this.acceleration;
         }
         if(this.controls.reverse) {
-            this.y += 2;
+            this.speed -= this.acceleration;
         }
-        if(this.controls.left) {
-            this.x -= 2;
+        if(this.speed > this.maxSpeed) {
+            this.speed = this.maxSpeed;
         }
-        if(this.controls.right) {
-            this.x += 2;
+        if(this.speed < -this.maxSpeed/2) {
+            this.speed == -this.maxSpeed/2;
         }
+        
+        this.y -= this.speed;
+
     }
 
     draw(ctx) {
