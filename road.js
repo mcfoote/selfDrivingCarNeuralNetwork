79f4eca29ba4e31f105cc37dd680772168632a0d
Mitchell Foote <mcfoote321@gmail.com>
2022-10-11 01:10:55 -0400

    add utils

--- road.js
+++ road.js
@@ -1,6 +1,6 @@
 class Road {
 
-    constructor(x, width, laneCount = 3) {
+    constructor(x, width, laneCount = 4) {
 
         this.x = x;
         this.width = width;
@@ -15,33 +15,38 @@ class Road {
 
     }
 
+    getLaneCenter(laneIndex) {
+        const lineWidth = this.width/this.laneCount;
+        return this.left + this.lineWidth/2 + laneIndex*lineWidth;
+    }
+
     draw(ctx) {
 
         ctx.lineWidth = 5;
         ctx.strokesStyle = 'white';
 
         for(let i = 0; i <= this.laneCount; i++) {
+
             const x = lerp (
                 this.left,
                 this.right,
                 i/this.laneCount
             );
+            
+            if(i > 0 && i < this.laneCount) {
+                ctx.setLineDash([20, 20]);
+            }else {
+                ctx.setLineDash([]);
+            }
+
+            ctx.beginPath();
+            ctx.moveTo(x, this.top);
+            ctx.lineTo(x, this.bottom);
+            ctx.stroke();
+            
         }
-
-        ctx.beginPath();
-        ctx.moveTo(this.left, this.top);
-        ctx.lineTo(this.left, this.bottom);
-        ctx.stroke();
-
-        ctx.beginPath();
-        ctx.moveTo(this.right, this.top);
-        ctx.lineTo(this.right, this.bottom);
-        ctx.stroke();
 
     }
 
 }
 
-function lerp(A, B, t) {
-    return A + (B - A) * t;
-}
