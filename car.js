aeeeacde63267fb46a3920fb1e8697978d5b54fa
Mitchell Foote <mcfoote321@gmail.com>
2023-02-16 18:15:10 -0500

    work on controls & physics

--- car.js
+++ car.js
@@ -11,6 +11,7 @@ class Car {
         this.acceleration = 0.2;
         this.maxSpeed = 3;
         this.friction = 0.05;
+        this.angle = 0;
 
         this.controls = new Controls();
 
@@ -32,31 +33,46 @@ class Car {
             this.speed = -this.maxSpeed/2;
         }
 
-        this.y -= this.speed;
-        /*
+        if(this.speed > 0) {
+            this.speed -= this.friction;
+        }
+        if(this.speed < 0) {
+            this.speed += this.friction;
+        }
+        if(Math.abs(this.speed) < this.friction) {
+            this.speed = 0;
+        }
+       
         if(this.controls.left) {
-            this.x -= 2;
+            this.angle += 0.03;
         }
         if(this.controls.right) {
-            this.x += 2;
+            this.angle -= 0.03;
         }
-        */
+
+        this.y -= this.speed;
+        
     }
 
     draw(ctx) {
 
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(-this.angle);
         ctx.beginPath();
 
         ctx.rect(
 
-            this.x-this.width/2,
-            this.y-this.height/2,
+            -this.width/2,
+            -this.height/2,
             this.width,
             this.height
 
         );
 
         ctx.fill();
+
+        ctx.restore();
     }
 
 }
