2a156acbec9141deb8a4be2084b00bac7afa702f
Mitchell Foote <mcfoote321@gmail.com>
2023-02-16 18:01:23 -0500

    work on physics

--- car.js
+++ car.js
@@ -9,6 +9,8 @@ class Car {
 
         this.speed = 0;
         this.acceleration = 0.2;
+        this.maxSpeed = 3;
+        this.friction = 0.05;
 
         this.controls = new Controls();
 
@@ -21,6 +23,13 @@ class Car {
         }
         if(this.controls.reverse) {
             this.speed -= this.acceleration;
+        }
+
+        if(this.speed > this.maxSpeed) {
+            this.speed = this.maxSpeed;
+        }
+        if(this.speed < -this.maxSpeed/2) {
+            this.speed = -this.maxSpeed/2;
         }
 
         this.y -= this.speed;
