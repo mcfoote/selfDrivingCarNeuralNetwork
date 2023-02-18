bc3dcf3f7591ac8e91a6a9558b2feffb88173f15
Mitchell Foote <mcfoote321@gmail.com>
2023-02-17 23:12:51 -0500

    work on road

--- road.js
+++ road.js
@@ -1,6 +1,6 @@
 class Road {
 
-    constructor(x, width, laneCount=4) {
+    constructor(x, width, laneCount=3) {
 
         this.x = x;
         this.width = width;
@@ -13,6 +13,20 @@ class Road {
         this.top = -infinity;
         this.bottom = infinity;
 
+        const topLeft = {x:this.left, y:this.top};
+        const topRight = {x:this.right, y:this.top};
+        const bottomLeft = {x:this.left, y:this.bottom};
+        const bottomRight = {x:this.right, y:this.bottom};
+        this.borders = [
+            [topLeft, bottomLeft],
+            [topRight, bottomRight]
+        ]
+    }
+
+    getLaneCenter(laneIndex) {
+        const lineWidth = this.width/this.laneCount;
+        return this.left + lineWidth/2 + 
+            Math.min(laneIndex, this.laneCount-1)*lineWidth;
     }
 
     draw(ctx) {
@@ -20,7 +34,7 @@ class Road {
         ctx.lineWidth = 5;
         ctx.strokeStyle = "white";
 
-        for(let i=0; i <= this.laneCount; i++) {
+        for(let i=1; i <= this.laneCount-1; i++) {
             const x = lerp(
                 this.left,
                 this.right,
