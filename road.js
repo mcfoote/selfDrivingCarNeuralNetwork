0b5948cb4a93b42c192c356aab08dcc237bffbdc
Mitchell Foote <mcfoote321@gmail.com>
2023-02-17 22:38:10 -0500

    work on road

--- road.js
+++ road.js
@@ -1,6 +1,6 @@
 class Road {
 
-    constructor(x, width, laneCount=3) {
+    constructor(x, width, laneCount=4) {
 
         this.x = x;
         this.width = width;
@@ -20,16 +20,24 @@ class Road {
         ctx.lineWidth = 5;
         ctx.strokeStyle = "white";
 
-        ctx.beginPath();
-        ctx.moveTo(this.left, this.top);
-        ctx.lineTo(this.left, this.bottom);
-        ctx.stroke();
+        for(let i=0; i <= this.laneCount; i++) {
+            const x = lerp(
+                this.left,
+                this.right,
+                i/this.laneCount
+            );
+                
+            if(i>0 && i < this.laneCount) {
+                ctx.setLineDash ([20, 20]);
+            } else {
+                ctx.setLineDash([]);
+            }
+            ctx.beginPath();
+            ctx.moveTo(x, this.top);
+            ctx.lineTo(x, this.bottom);
+            ctx.stroke();
 
-        ctx.beginPath();
-        ctx.moveTo(this.right, this.top);
-        ctx.lineTo(this.right, this.bottom);
-        ctx.stroke();
-
+        }
     }
 
 }
