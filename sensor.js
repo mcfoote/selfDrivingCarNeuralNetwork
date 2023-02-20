c8128df8939aa9677da3d8386f8baf2abdcd5cf4
Mitchell Foote <mcfoote321@gmail.com>
2023-02-20 16:55:15 -0500

    work on sensor

--- sensor.js
+++ sensor.js
@@ -47,6 +47,13 @@ class Sensor {
                 this.rays[i][0].x,
                 this.rays[i][0].y
             );
+
+            ctx.lineTo(
+                this.rays[i][1].x,
+                this.rays[i][1].y
+            );
+
+            ctx.stroke();
         }
 
     }
