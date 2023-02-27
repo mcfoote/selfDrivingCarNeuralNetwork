6445712060aa23049e459867fc37e610950b7fe2
Mitchell Foote <mcfoote321@gmail.com>
2023-02-26 22:11:58 -0500

    rework sensor

--- sensor.js
+++ sensor.js
@@ -7,7 +7,29 @@ class Sensor {
         this.rayLength = 100;
         this.raySpread = Math.PI / 4;
 
-        this.rays=[];
+        this.rays = [];
+
+    }
+
+    update() {
+
+        this.rays = [];
+
+        for(let i=0; i< this.rayCount; i++) {
+            const rayAngle = lerp(
+                this.raySpread/2,
+                -this.raySpread/2,
+                i/(this.rayCount -1)
+            );
+
+            const start = {x:this.car.x, y:this.car.y};
+            const end = {
+                x:this.car.x-
+                    Math.sin(rayAngle)*this.rayLength,
+                y:this.car.y-
+                    Math.cos(rayAngle)*this.rayLength
+            };
+        }
 
     }
 
