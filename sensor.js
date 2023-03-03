f7221022dae293d5824cce9ccae46ba75c3d370c
Mitchell Foote <mcfoote321@gmail.com>
2023-03-02 22:19:59 -0500

    work on sensor

--- sensor.js
+++ sensor.js
@@ -42,7 +42,9 @@ class Sensor {
         if((touches.length == 0)){
             return null;
         } else {
-            
+            const offsets = touches.map(e => e.offset);
+            const minOffset = Math.min(...offsets);
+            return touches.find(e => e.offset == minOffset);
         }
 
     }
