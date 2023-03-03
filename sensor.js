0abc561542666173858d44d64f3b78f4bc705bc9
Mitchell Foote <mcfoote321@gmail.com>
2023-03-02 22:22:26 -0500

    work on sensor

--- sensor.js
+++ sensor.js
@@ -74,6 +74,10 @@ class Sensor {
     draw(ctx) {
 
         for(let i=0; i<this.rayCount; i++) {
+            let end = this.rays[i][1];
+            if(this.reading[i]) {
+                end = this.readings[i];
+            }
             ctx.beginPath();
             ctx.lineWidth = 2;
             ctx.strokeStyle = "yellow";
