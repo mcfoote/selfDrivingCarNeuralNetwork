843ff79bd630b34e8bb01dc1d2f948ea892b95dc
Mitchell Foote <mcfoote321@gmail.com>
2023-03-08 14:43:23 -0500

    work on traffic

--- car.js
+++ car.js
@@ -1,6 +1,6 @@
 class Car {
 
-    constructor(x, y, width, height, controlType) {
+    constructor(x, y, width, height, controlType, maxSpeed=3) {
 
         this.x=x;
         this.y=y;
@@ -9,7 +9,7 @@ class Car {
 
         this.speed = 0;
         this.acceleration = 0.2;
-        this.maxSpeed = 3;
+        this.maxSpeed = maxSpeed;
         this.friction = 0.05;
         this.angle = 0;
         this.damaged = false;
