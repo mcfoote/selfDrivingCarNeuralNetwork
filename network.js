c7ef36cc5a1fbadb8715bfaaadd9880c08b9c6fb
Mitchell Foote <mcfoote321@gmail.com>
2023-03-10 15:13:49 -0500

    work on network

---
+++ network.js
@@ -0,0 +1,15 @@
+class Level {
+
+    constructor(inputCount, outputCount) {
+
+        this.inputs = new Array(inputCount);
+        this.outputs = new Array(outputCount);
+        this.biases = new Array(outputCount);
+
+        this.weights = [];
+        for(let i = 0; i < inputCount; i++) {
+            this.weights[i] = new Array(outputCount);
+        }
+    }
+
+}
