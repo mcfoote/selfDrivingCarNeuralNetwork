6ed68941aa34e7e969cc444f51a5ed00c3237c5a
Mitchell Foote <mcfoote321@gmail.com>
2023-03-19 15:22:02 -0400

    work on network

--- main.js
+++ main.js
@@ -4,9 +4,11 @@ carCanvas.width = 200;
 
 const networkCanvas = document.getElementById("networkCanvas");
 
-carCanvas.width = 200;
+networkCanvas.width = 300;
 
 const carCtx = carCanvas.getContext("2d");
+const networkCtx = networkCanvas.getContext("2d");
+
 const road = new Road(carCanvas.width/2, carCanvas.width*0.9);
 const car = new Car(road.getLaneCenter(1), 100, 30, 50, "AI");
 const traffic = [
@@ -24,6 +26,7 @@ function animate() {
     car.update(road.borders, traffic);
 
     carCanvas.height = window.innerHeight;
+    networkCanvas.height = window.innerHeight;
 
     carCtx.save();
     carCtx.translate(0, -car.y + carCanvas.height * 0.7);
