064f6de6807c9370bb7da9b616317d3a8a1c0d80
Mitchell Foote <mcfoote321@gmail.com>
2023-03-22 18:54:50 -0400

    work on visualizer

--- visualizer.js
+++ visualizer.js
@@ -25,7 +25,7 @@ class Visualizer {
                 left,levelTop,
                 width,levelHeight,
                 i==network.levels.length-1
-                    ?['up','left','right','down']
+                    ?['^','<','>','v']
                     :[]
             );
         }
