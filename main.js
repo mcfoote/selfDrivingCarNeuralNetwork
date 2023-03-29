31d6b1c07c4d874754dd498994c03bf2ce83101e
Mitchell Foote <mcfoote321@gmail.com>
2023-03-29 00:18:45 -0400

    work on genetic algorithm

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ if(localStorage.getItem("bestBrain")) {
             localStorage.getItem("bestBrain")
         );
         if(i != 0){
-            NeuralNetwork.mutate(cars[i].brain, 0.3);
+            NeuralNetwork.mutate(cars[i].brain, 0.2);
         }
     }
 
@@ -32,6 +32,10 @@ const traffic = [
     new Car(road.getLaneCenter(0), -300, 30, 50, "DUMMY", 2),
     new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2),
     new Car(road.getLaneCenter(2), -300, 30, 50, "DUMMY", 2),
+    new Car(road.getLaneCenter(0), -500, 30, 50, "DUMMY", 2),
+    new Car(road.getLaneCenter(1), -500, 30, 50, "DUMMY", 2),
+    new Car(road.getLaneCenter(1), -750, 30, 50, "DUMMY", 2),
+    new Car(road.getLaneCenter(2), -750, 30, 50, "DUMMY", 2),
 ];
 
 animate();
