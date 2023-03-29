62460c33356df1444cdb692b8e52255a627f7b0e
Mitchell Foote <mcfoote321@gmail.com>
2023-03-29 00:04:06 -0400

    work on genetic algorithm

--- network.js
+++ network.js
@@ -26,7 +26,9 @@ class NeuralNetwork {
     }
 
     static mutate(network, amount=1) {
+
         network.levels.forEach(level => {
+
             for(let i = 0; i < level.biases.length; i++) {
                 level.biases[i] = lerp(
                     level.biases[i],
@@ -34,8 +36,19 @@ class NeuralNetwork {
                     amount
                 )
             }
+
+            for(let i = 0; i < level.weights.length; i++) {
+                for(let j = 0; j < level.weights[i].length; j++) {
+                    level.weights[i][j] = lerp(
+                        level.weights[i][j],
+                        Math.random()*2-1,
+                        amount
+                    )
+                }
+            }
             
-        })
+        });
+
     }
 
 }
